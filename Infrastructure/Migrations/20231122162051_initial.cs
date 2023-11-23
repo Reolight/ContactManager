6865d5b0256f5f6c15e6fab4fb840ff4880ff5e0
Reolight <95131769+Reolight@users.noreply.github.com>
2023-11-23 17:26:42 +0300

    Migrations added

---
+++ Infrastructure/Migrations/20231122162051_initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    MobilePhone = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    JobTitle = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
